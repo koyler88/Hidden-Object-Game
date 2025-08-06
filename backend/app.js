62deaa1e84efd6408dd6e4aa70a486eb4ed810b1
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-08-06 11:46:09 -0600

    feat(cors): configure cors for frontend port

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors')
+app.use(cors({ origin: 'http://localhost:5173'}))
 
 // Routers
 const gamesRouter = require("./routes/gamesRouter");
