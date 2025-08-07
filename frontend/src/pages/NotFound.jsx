37da81987c37ed3c891d36bc1f6c440e9c9d2a42
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-08-07 15:34:48 -0600

    feat(frontend): create basic pages for Home, Game, and NotFound routes

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+      <h1>404</h1>
+      <p>Page Not Found</p>
+    </div>
+  );
+}
+
+export default NotFound;
